7b9f69f838b1acf70f5b7179eead4b64f079f48a
Hatchino <hemme.lea@gmail.com>
2023-04-19 22:52:33 +0200

    Change in code structure

---
+++ form.js
@@ -0,0 +1,83 @@
+const form = document.forms["form-contact"];
+const submit = form.querySelector('input[type=submit]');
+const errorMessages = form.querySelectorAll('.error-message');
+const errorIcon = document.querySelector('.error-icon');
+
+
+// Event listener for form submission
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let tabError = [];
+    checkForm(tabError);
+    //setError();
+
+    if (tabError.length === 0) {
+        location.reload();
+    }
+
+})
+
+const checkForm = (tabError) => {
+   
+    for (let objetForm of form) {
+        let type = objetForm.type;
+        let name = objetForm.name;
+        let value = objetForm.value;
+
+        if (type == "submit") {
+            continue;
+        }
+
+
+        // Add custom error message when the value is empty
+        if (value == "") {
+            switch (name) {
+                case "first-name":
+                    tabError.push("First Name cannot be empty");
+                    objetForm.classList.add("error");
+                    errorIcon.style.display = 'block';
+                    break;
+                case "last-name":
+                    tabError.push("Last Name cannot be empty");
+                    objetForm.classList.add("error");
+                    errorIcon.style.display = 'block';
+                    break;
+                case "password":
+                    tabError.push("Password cannot be empty");
+                    objetForm.classList.add("error");
+                    errorIcon.style.display = 'block';
+                    break;
+
+                case "email":
+                    // Check if the email address is formatted correctly
+                    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+                    if (!emailRegex.test(value)) {
+                        tabError.push("Looks like this is not an email");
+                        objetForm.classList.add("error");
+                        errorIcon.style.display = 'block';
+                    }
+                    break;
+
+                default:
+                    tabError.push(`${name} cannot be empty`);
+            }
+        }
+
+        const divError = objetForm.nextElementSibling;
+
+        if (!divError || !divError.classList.contains("error-message")) {
+             // Create an error element to display the error message
+             const errorElement = document.createElement("span");
+             errorElement.className = "error-message";
+             errorElement.innerText = tabError[tabError.length - 1];
+ 
+             // Insert the error element after the form field
+             objetForm.parentNode.insertBefore(errorElement, objetForm.nextSibling);
+         }
+    }
+}
+
+
+
